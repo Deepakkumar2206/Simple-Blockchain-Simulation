73a57efe4fa14ff52e7590e47ebaa24b9e38827c
Deepakkumar <deepakjkumar622@gmail.com>
2025-01-26 12:21:57 +0530

    Add project files

--- truffle-config.js
+++ truffle-config.js
@@ -7,13 +7,13 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 7545, // Change this if you're using a different port
-      network_id: "*", // Match any network id
+      port: 7545, 
+      network_id: "*", 
     },
   },
   compilers: {
     solc: {
-      version: "0.8.0", // Specify the Solidity version
+      version: "0.8.0", 
     },
   },
 };
